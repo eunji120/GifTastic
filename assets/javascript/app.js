0f331851b1b94ef59bb68771b612baf429618881
eunji120 <eunji120@gmail.com>
2018-06-25 22:01:36 -0400

    Edited files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,87 @@
 //initial array of people
 var people = ["Drake", "Barak Obama", "Donald Trump", "Oprah Winfrey", "Justin Bieber", "Beyoncé", "Madonna", "Kim Kardashian", "Sarah Palin", "Arnold Schwarzenegger"];
 
-//displayPeopleGif function re-renders the HTML to show the corresponding gifs
-function displayPeopleGif() {
+
+$("#buttons-view").on("click", function() {
+    var thing = $(this).attr("data");
+    var queryURL = "http://api.giphy.com/v1/gifs/?t=" + person + "famouse+people&api_key=cZVFvzDv27P3cu5wX8rSTV1gYTrDAJ6Q";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+            //creating a div to hold the person
+            var personDiv = $("<div class='person'>");
+            //storing the rating data
+            var rating = response.Rated;
+            //creating an element to have the rating shown
+            var p = $("<p>").text("Rating: " + rating);
+            //showing the rating
+            personDiv.append(p);
+
+            var gifURL = response.Gif;
+            var gif = $(".gif").attr("src", gifURL);
+            personDiv.append(gif);
+            $("#people-view").prepend(personDiv) 
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// //displayPeopleGif function re-renders the HTML to show the corresponding gifs
+// function displayPeopleGif() {
     
-    var 
+//     var person = $(this).attr("data-name");
+//     var queryURL = "http://api.giphy.com/v1/gifs/?t=" + person + "famouse+people&api_key=cZVFvzDv27P3cu5wX8rSTV1gYTrDAJ6Q";
+
+//     //AJAX call for the specific individual
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(response) {
+//         console.log(response);
+//         //creating a div to hold the person
+//         var personDiv = $("<div class='person'>");
+//         //storing the rating data
+//         var rating = response.Rated;
+//         //creating an element to have the rating shown
+//         var pOne = $("<p>").text("Rating: " + rating);
+//         //showing the rating
+//         personDiv.append(pOne);
+
+//         var gifURL = response.Gif;
+//         var gif = $(".gif").attr("src", gifURL);
+//         personDiv.append(gif);
+//         $("#people-view").prepend(personDiv) 
+//     });
+// }
+
+
+
+
+var button;
+
+var renderButtons = function () {
+    $("#add-person").empty();
+
+    for (var i = 0; i < people.length; i++) {
+        var 
+    }
+
 }
+
