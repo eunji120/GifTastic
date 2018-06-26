b99ff6c9bba34d5a3f0014e899b7044b14934cb2
eunji120 <eunji120@gmail.com>
2018-06-25 20:27:37 -0400

    Edited files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,8 @@
-var animal;
+//initial array of people
+var people = ["Drake", "Barak Obama", "Donald Trump", "Oprah Winfrey", "Justin Bieber", "Beyoncé", "Madonna", "Kim Kardashian", "Sarah Palin", "Arnold Schwarzenegger"];
 
+//displayPeopleGif function re-renders the HTML to show the corresponding gifs
+function displayPeopleGif() {
+    
+    var 
+}
